f9f4bed661309002a5c2d8239a700400bb40a7cf
ranjeetjangid08 <141650986+ranjeetjangid08@users.noreply.github.com>
2024-10-17 16:30:07 +0530

    add new cv

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Ranjeet-resume.pdf";
+import CV from "../../assets/Ranjeet-Resume.pdf";
 import Info from "./Info.jsx";
 
 const About = () => {
