c91e7dd1492b6fb5d8156b8b7ceff3419480a2be
ranjeetjangid08 <141650986+ranjeetjangid08@users.noreply.github.com>
2024-10-17 16:38:27 +0530

    rename cv pdf file

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Ranjeet-Resume.pdf";
+import CV from "../../assets/Ranjeet_Resume.pdf";
 import Info from "./Info.jsx";
 
 const About = () => {
